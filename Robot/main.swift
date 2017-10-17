7067fe3e0d989518cd8023a2e475b0e5d14e4ecd
Lucien <lucienfaure79@gmail.com>
2017-10-17 19:37:03 +0200

    Initial Commit

---
+++ Robot/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Robot
+//
+//  Created by Lucien Faure on 17/10/2017.
+//  Copyright © 2017 Lucien Faure. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
