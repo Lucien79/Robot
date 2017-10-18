594fecdb6f02c900dae41ab4a89b11947b2790ad
Lucien <lucienfaure79@gmail.com>
2017-10-18 21:39:51 +0200

    Ajout de la méthode seDeplacer avec une erreur sur l'initialisation  de l'énumération directionHBDG

--- Robot/Robot.swift
+++ Robot/Robot.swift
@@ -1,4 +1,5 @@
 class Robot {
+    
     var nom: String
     var vie = 100
     var vitMaxDeplacement = 3
@@ -11,6 +12,37 @@ class Robot {
     
     func sePresenter()  {
         print("Bonjour je m'appelle \(nom)). J'ai \(vie) point de vie. Je me déplace à \(vitMaxDeplacement) cases par seconde. Je suis à la case de coordonnées \(position).")
+    }
+    
+    func seDeplacer(nombreCase: Int)  {
+        
+        enum directionHBDG {
+        case haut, bas, droite, gauche
+        }
+       
+        var direction: directionHBDG
+        init(direction: directionHBDG ) {
+            self.direction = direction
+        }
+        
+        if nombreCase <= vitMaxDeplacement {
+        
+            switch direction {
+                case.haut:
+                    position = (x: 0, y: +nombreCase)
+            
+                case.bas:
+                    position = (x:0, y: -nombreCase)
+            
+                case.droite:
+                    position = (x: +nombreCase, y: 0)
+            
+                case.gauche:
+                    position = (x: -nombreCase, y:0)
+            }
+        }else {
+            print("Le deplacement maximal est de \(vitMaxDeplacement). Veuillez choisir un nombre entre 1 et 3")
+        }
     }
 }
 
--- Robot/main.swift
+++ Robot/main.swift
@@ -5,11 +5,12 @@ var robotAmi2 = Robot(nom: "Sanji")
 
 var robots: [Robot] = [robot,robotAmi,robotAmi1,robotAmi2]
 
-
+robot.seDeplacer(nombreCase: 3)
 for _ in 1...3{
     for unRobot in robots {
         unRobot.sePresenter()
     }
 }
-    
+
+
 
