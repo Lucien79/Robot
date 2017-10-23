da8b44cbdf6f6bd4d66b216936334959f8434a27
Lucien <lucienfaure79@gmail.com>
2017-10-23 13:42:17 +0200

    correction de la méthode seDeplacer() en ajoutant le paramètre direction de type Direction / création de la méthode se DeplacerAleatoirement() + ajout propriétés random

--- Robot/Robot.swift
+++ Robot/Robot.swift
@@ -1,48 +1,73 @@
+import Foundation
 class Robot {
-    
+   
+    enum Direction {
+        case haut, bas, droite, gauche
+    }
     var nom: String
-    var vie = 100
-    var vitMaxDeplacement = 3
-    var position = (x: 0, y: 0)
+    var vie: Int
+    var vitMaxDeplacement : Int
+    var position : (x: Int, y: Int)
+    var nombreCase = Int(arc4random_uniform(3)) + 1
+    var tabDirection: [Direction] = [Direction.bas, Direction.gauche, Direction.haut, Direction.droite]
     
-    
-    init(nom: String) {
+    init(nom: String, vie: Int, vitMaxDeplacement: Int, position:(x:Int,y: Int)) {
         self.nom = nom
+        self.vie = vie
+        self.vitMaxDeplacement = vitMaxDeplacement
+        self.position = position
+        
     }
     
     func sePresenter()  {
         print("Bonjour je m'appelle \(nom)). J'ai \(vie) point de vie. Je me déplace à \(vitMaxDeplacement) cases par seconde. Je suis à la case de coordonnées \(position).")
     }
     
-    func seDeplacer(nombreCase: Int)  {
+    
+    func seDeplacer(nombreCase: Int, direction: Direction)  {
         
-        enum directionHBDG {
-        case haut, bas, droite, gauche
-        }
-       
-        var direction: directionHBDG
-        init(direction: directionHBDG ) {
-            self.direction = direction
-        }
-        
+        for _ in 1...vitMaxDeplacement {
         if nombreCase <= vitMaxDeplacement {
         
             switch direction {
-                case.haut:
+                case .haut:
                     position = (x: 0, y: +nombreCase)
             
-                case.bas:
+                case .bas:
                     position = (x:0, y: -nombreCase)
             
-                case.droite:
+                case .droite:
                     position = (x: +nombreCase, y: 0)
             
-                case.gauche:
+                case .gauche:
                     position = (x: -nombreCase, y:0)
             }
         }else {
             print("Le deplacement maximal est de \(vitMaxDeplacement). Veuillez choisir un nombre entre 1 et 3")
         }
     }
+    }
+    
+    func seDeplacerAleatoirement(repetition: Int)  {
+        let randomIndex = Int(arc4random_uniform(UInt32(tabDirection.count)))
+        let direction = tabDirection[randomIndex]
+        
+        for _ in 1...repetition {
+            if nombreCase <= vitMaxDeplacement {
+            
+                if direction == Direction.haut {
+                    position = (x: 0, y: +nombreCase)
+                }else if direction == Direction.bas {
+                    position = (x:0, y: -nombreCase)
+                }else if direction == Direction.droite {
+                    position = (x: +nombreCase, y: 0)
+                }else if direction == Direction.gauche {
+                    position = (x: -nombreCase, y:0)
+                }
+           }
+        }
+    }
 }
+
+
 
--- Robot/main.swift
+++ Robot/main.swift
@@ -1,14 +1,16 @@
-var robot = Robot(nom: "Luffy")
-var robotAmi = Robot(nom: "Zoro")
-var robotAmi1 = Robot(nom: "Nami")
-var robotAmi2 = Robot(nom: "Sanji")
+var robot = Robot(nom: "Luffy", vie: 200, vitMaxDeplacement: 6, position: (x: 5, y:10))
+var robotAmi = Robot(nom: "Zoro", vie: 100, vitMaxDeplacement: 10, position: (x: 10, y: 20))
+var robotAmi1 = Robot(nom: "Nami", vie: 150, vitMaxDeplacement: 2, position: (x: 0, y:0))
+var robotAmi2 = Robot(nom: "Sanji", vie: 250, vitMaxDeplacement: 5, position: (x: 2, y: 3))
 
 var robots: [Robot] = [robot,robotAmi,robotAmi1,robotAmi2]
 
-robot.seDeplacer(nombreCase: 3)
-for _ in 1...3{
+
+for _ in 1...2{
     for unRobot in robots {
+        unRobot.seDeplacerAleatoirement(repetition: 5)
         unRobot.sePresenter()
+        print(unRobot.nombreCase)
     }
 }
 
